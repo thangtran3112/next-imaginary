fb70893153c55d806b5c4bf5d88c72cf13725bf8
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-08-11 09:03:23 -0700

    display upload image and add toast to layout

--- webapp/components/shared/MediaUploader.tsx
+++ webapp/components/shared/MediaUploader.tsx
@@ -1,6 +1,10 @@
+"use client";
+
+import { dataUrl, getImageSize } from "@/lib/utils";
 import { useToast } from "../ui/use-toast";
 import { CldImage, CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
+import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 
 type MediaUploaderProps = {
   onValueChange: (value: string) => void;
@@ -19,10 +23,21 @@ const MediaUploader = ({
   const { toast } = useToast();
 
   const onUploadSuccessHandler = (result: any) => {
+    setImage((prevState: any) => ({
+      ...prevState,
+      publicId: result?.info?.public_id,
+      width: result?.info?.width,
+      height: result?.info?.height,
+      secureURL: result?.info?.secure_url,
+    }));
+
+    //sync the new public_id and show the image
+    onValueChange(result?.info?.public_id);
+
     toast({
       title: "Image uploaded successfully",
       description: "1 credit was deducted from your account",
-      duration: 5000,
+      duration: 3000,
       className: "success-toast",
     });
   };
@@ -30,7 +45,7 @@ const MediaUploader = ({
     toast({
       title: "Something went wrong while uploading",
       description: "Please try again",
-      duration: 5000,
+      duration: 3000,
       className: "error-toast",
     });
   };
@@ -49,7 +64,19 @@ const MediaUploader = ({
         <div className="flex flex-col gap-4">
           <h3 className="h3-bold text-dark-600">Original</h3>
           {publicId ? (
-            <>HERE IS THE IMAGE</>
+            <>
+              <div className="cursor-pointer overflow-hidden rounded-[10px]">
+                <CldImage
+                  width={getImageSize(type, image, "width")}
+                  height={getImageSize(type, image, "height")}
+                  src={publicId}
+                  alt="image"
+                  sizes={"(max-width: 767px) 100vw, 50vw"}
+                  placeholder={dataUrl as PlaceholderValue}
+                  className="media-uploader_cldImage"
+                />
+              </div>
+            </>
           ) : (
             <div className="media-uploader_cta" onClick={() => open()}>
               <div className="media-uploader_cta-image">
